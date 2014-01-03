75ea4e9bbb2f1d87cb3924caeb1be3b257fc2b7e
Dave Brosius <dbrosius@mebigfatguy.com>
2014-01-03 01:18:36 -0500

    add preferredSize sub-element

--- src/com/mebigfatguy/ds/DSContentHandler.java
+++ src/com/mebigfatguy/ds/DSContentHandler.java
@@ -2,6 +2,7 @@ package com.mebigfatguy.ds;
 
 import java.awt.Component;
 import java.awt.Container;
+import java.awt.Dimension;
 import java.awt.LayoutManager;
 import java.lang.reflect.Field;
 import java.lang.reflect.Method;
@@ -18,6 +19,7 @@ public class DSContentHandler<T extends RootPaneContainer> extends DefaultHandle
 
     private static final String CONTAINER = "container";
     private static final String CONTROL = "control";
+    private static final String PREFERRED_SIZE = "preferredSize";
     
     private static final String ATTR_NAME = "name";
     private static final String ATTR_TITLE = "title";
@@ -28,6 +30,8 @@ public class DSContentHandler<T extends RootPaneContainer> extends DefaultHandle
     private DSLocalizer localizer;
     private Component topComponent;
     private List<Container> containerStack = new ArrayList<>();
+    private Component activeComponent = null;
+    private StringBuilder pcData = new StringBuilder();
     
     public DSContentHandler(DSLocalizer l10n) {
         localizer = l10n;
@@ -39,38 +43,41 @@ public class DSContentHandler<T extends RootPaneContainer> extends DefaultHandle
 
     @Override
     public void startElement(String uri, String localName, String qName, Attributes attributes) throws SAXException {
-        Component c;
         try {
-            String type = attributes.getValue(ATTR_TYPE);
-            Class<?> cls = Class.forName(type);
-            c = (Component) cls.newInstance();
-            
-            String name = attributes.getValue(ATTR_NAME);
-            if (name != null) {
-                setName(c, name);
-            }
-            
-            String title = attributes.getValue(ATTR_TITLE);
-            if (title != null) {
-                setTitle(c, localizer.getLocalString(title));
-            }
-            
-            Container parent = null;
-            if (!containerStack.isEmpty()) {
-                parent = containerStack.get(containerStack.size() - 1);
-            }
-
-            if (qName.equals(CONTAINER)) {
-                String layout = attributes.getValue(ATTR_LAYOUT);
-                if (layout != null) {
-                    cls = Class.forName(layout);
-                    ((Container) c).setLayout((LayoutManager) cls.newInstance());
+            if (qName.equals(CONTAINER) || qName.equals(CONTROL)) {
+                String type = attributes.getValue(ATTR_TYPE);
+                Class<?> cls = Class.forName(type);
+                activeComponent = (Component) cls.newInstance();
+                
+                String name = attributes.getValue(ATTR_NAME);
+                if (name != null) {
+                    setName(activeComponent, name);
                 }
-                containerStack.add((Container) c);
-            }
-            
-            if (parent != null) {
-                addChild(parent, c, attributes.getValue(ATTR_POSITION));
+                
+                String title = attributes.getValue(ATTR_TITLE);
+                if (title != null) {
+                    setTitle(activeComponent, localizer.getLocalString(title));
+                }
+                
+                Container parent = null;
+                if (!containerStack.isEmpty()) {
+                    parent = containerStack.get(containerStack.size() - 1);
+                }
+    
+                if (qName.equals(CONTAINER)) {
+                    String layout = attributes.getValue(ATTR_LAYOUT);
+                    if (layout != null) {
+                        cls = Class.forName(layout);
+                        ((Container) activeComponent).setLayout((LayoutManager) cls.newInstance());
+                    }
+                    containerStack.add((Container) activeComponent);
+                }
+                
+                if (parent != null) {
+                    addChild(parent, activeComponent, attributes.getValue(ATTR_POSITION));
+                }
+            } else {
+                pcData.setLength(0);
             }
         } catch (Exception e) {
             //what to do?
@@ -81,11 +88,22 @@ public class DSContentHandler<T extends RootPaneContainer> extends DefaultHandle
     public void endElement(String uri, String localName, String qName) throws SAXException {
         if (qName.equals(CONTAINER)) {
             topComponent = containerStack.remove(containerStack.size() - 1);
+            activeComponent = topComponent;
+        } else if (qName.equals(CONTROL)) {
+            activeComponent = containerStack.get(containerStack.size() - 1);
+        } else {
+            if (qName.equals(PREFERRED_SIZE)) {
+                String[] sizes = pcData.toString().split(",");
+                Dimension d = new Dimension(Integer.parseInt(sizes[0]), Integer.parseInt(sizes[1]));
+                activeComponent.setPreferredSize(d);
+            }
+            pcData.setLength(0);
         }
     }
 
     @Override
     public void characters(char[] ch, int start, int length) throws SAXException {
+        pcData.append(ch, start, length);
     }
     
     private void addChild(Container parent, Component child, String position) {
