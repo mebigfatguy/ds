c1f500171b509e862df5303afde24f9365a674c7
Dave Brosius <dbrosius@mebigfatguy.com>
2015-07-23 22:56:58 -0400

    fix some schema validation problems

--- src/com/mebigfatguy/ds/spi/FrameDSProvider.java
+++ src/com/mebigfatguy/ds/spi/FrameDSProvider.java
@@ -46,7 +46,7 @@ public class FrameDSProvider extends AbstractDSProvider {
 
 	@Override
 	public void startElement(String uri, String localName, String qName, Attributes attributes) throws SAXException {
-		if ("frame".equals(localName)) {
+		if ("Frame".equals(localName)) {
 			frame = new JFrame();
 		}
 	}
